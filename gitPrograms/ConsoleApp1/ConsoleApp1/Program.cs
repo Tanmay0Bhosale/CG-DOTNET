bed336f8093b1bf901e39bc2cf77110345fd674c
Tanmay <tanmaytanvi1234@gmail.com>
2025-12-16 15:17:27 +0530

    Addded Calculator Program in feature/Branch

---
+++ gitPrograms/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose operation (+, -, *, /):");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            double result = 0;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    if (num2 != 0)
+                        result = num1 / num2;
+                    else
+                    {
+                        Console.WriteLine("Division by zero is not allowed.");
+                        return;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operator");
+                    return;
+            }
+
+            Console.WriteLine($"Result = {result}");
+        }
+    }
+}
